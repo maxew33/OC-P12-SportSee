a4343991c82fd3f64f33895905ef5a4847ad5ff3
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-06-19 11:31:10 +0200

    :lipstick: [DashBoard] grid created

--- src/components/navbarHor/NavBarHor.tsx
+++ src/components/navbarHor/NavBarHor.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import Logo from '../../assets/logo.svg'
+import styles from './NavBarHor.module.css'
 
 export default function NavBarHor() {
-  return (
-    <div>NavBarHor</div>
-  )
+    return (
+        <nav className={styles.navBar}>
+            <img src={Logo} alt="SportSee logo" className={styles.logo}/>
+            <ul className={styles.navLinks}>
+                <li>Accueil</li>
+                <li>Profil</li>
+                <li>Réglage</li>
+                <li>Communauté</li>
+            </ul>
+        </nav>
+    )
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,10 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
+import Models from '../../models/models'
+
 import NavBarHor from '../../components/navbarHor/NavBarHor'
 import NavBarVert from '../../components/navbarVert/NavBarVert'
-
-import Models from '../../models/models'
 
 import ActivityChart from '../../components/charts/activity/ActivityChart'
 import MainChart from '../../components/charts/main/MainChart'
@@ -12,16 +12,14 @@ import PerformanceChart from '../../components/charts/performance/PerformanceCha
 import SessionsChart from '../../components/charts/sessions/SessionsChart'
 import Keydata from '../../components/charts/keyData/Keydata'
 
+import styles from './Dashboard.module.css'
+
 export default function Dashboard() {
     interface dataFormat {
         userMainData?: {
             score: number
             name: string
             keyData: { name: string; value: number; icon: string }[]
-            // kcal: number
-            // protein: number
-            // carboHydrate: number
-            // lipid: number
         }
 
         userActivity?: {
@@ -59,7 +57,6 @@ export default function Dashboard() {
 
     // Get the parameter from the URL
     const params = useParams()
-
     const userId = params.id
 
     useEffect(() => {
@@ -76,31 +73,28 @@ export default function Dashboard() {
         fetchData()
     }, [])
 
-    console.log(mainData)
-
     return (
-        <>
+        <main className={styles.dashboard}>
             {data && (
                 <>
-                    <main>
-                        <h1> dashboard - Bienvenue {mainData.name}</h1>
-                        <ActivityChart data={activitySession} />
-                        <MainChart data={mainData} />
-                        <PerformanceChart data={performance} />
-                        <SessionsChart data={sessions} />
-                        <div>
-                            {mainData.keyData.map((elt, idx) => (
-                                <div key={idx}>
-                                    <Keydata data={elt} />{' '}
-                                </div>
-                            ))}
-                        </div>
-                    </main>
-
-                    <NavBarHor />
-                    <NavBarVert />
+                    <h1> dashboard - Bienvenue {mainData.name}</h1>
+                    <ActivityChart data={activitySession} />
+                    <MainChart data={mainData} />
+                    <PerformanceChart data={performance} />
+                    <SessionsChart data={sessions} />
+                    <div>
+                        {mainData.keyData.map((elt, idx) => (
+                            <div key={idx}>
+                                <Keydata data={elt} />{' '}
+                            </div>
+                        ))}
+                    </div>
                 </>
             )}
-        </>
+
+            <NavBarHor />
+
+            <NavBarVert />
+        </main>
     )
 }
