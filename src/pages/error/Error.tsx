042b1f4d22248eaa360b53a42a371521e1c98b3c
Maxime Malfilatre <maxime.malfilatre@gmail.com>
2023-06-26 17:56:51 +0200

    :lipstick: [Error] 404 page done

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,7 +1,14 @@
+import styles from './Error.module.css'
+import { NavLink } from 'react-router-dom'
+
 export default function Error() {
     return (
-        <div>
-            404
+        <div className={styles.error}>
+            <span>404</span>
+            <br /> Oups, nous ne retrouvons pas la page.
+            <NavLink className={styles.link} to="/">
+                retour à la page de connexion
+            </NavLink>
         </div>
     )
 }
